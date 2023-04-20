a2ecae7eed3f3c367602b386590fa0fdf6af6691
yotamgod <yotamros@gmail.com>
2023-04-20 14:41:02 +0300

    Post-changes to modify test data without removing rows. Might still need to change it so the rows affected are exactly the same between test and train.

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -3,18 +3,34 @@ import os
 from IMLearn.utils import split_train_test
 from IMLearn.learners.regressors import LinearRegression
 
-from typing import NoReturn, Optional
+from typing import NoReturn, Optional, Tuple
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
+
+
+class TrainPreprocessMetadata:
+    """
+    A "dataclass" containing metadata about the training preprocess operation.
+    This is used to preprocess the test data.
+    """
+
+    def __init__(self) -> None:
+        self.col_median_values: Optional[pd.DataFrame] = None
+
+
+train_metadata = TrainPreprocessMetadata()
 
 
 def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     """
-    preprocess data
+    Preprocess sample data.
+    Preprocess is different for train data (y supplied) and test data (y not supplied).
+
     Parameters
     ----------
     X : DataFrame of shape (n_samples, n_features)
@@ -32,9 +48,8 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     # Make sure we don't ruin the entered data
     X = X.copy()
 
-    # Add "prices" to X to make it easier to handle indexes
-    if y is not None:
-        X['price'] = y
+    # If a y vector (prices) was supplied, the data inputted is training data.
+    is_train_data = y is not None
 
     # Remove unnecessary columns (ID)
     X = X.drop(["id", "lat", "long"], axis=1)
@@ -43,7 +58,28 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     X['date'] = pd.to_datetime(X['date'], format='%Y%m%dT', errors="coerce").apply(
         lambda x: x.timestamp() if pd.notna(x) else pd.NA)
 
-    # Remove empty rows and duplicates (there aren't any right here, but this should be taken care of)
+    if is_train_data:
+        X, y = _preprocess_train_data(X, y)
+
+    else:
+        X = _preprocess_test_data(X)
+
+    # Returning all values as floats
+    return X.astype(float), y
+
+
+def _preprocess_train_data(X: pd.DataFrame, y: pd.Series) -> Tuple[pd.DataFrame, pd.Series]:
+    """
+    Preprocess data for train data (rows can be removed, and metadata is saved for preprocessing test data).
+    :param X: DataFrame of shape (n_samples, n_features)
+              Design matrix of regression problem
+    :param y: array-like of shape (n_samples, )
+              Response vector corresponding given samples
+    :return: Preprocessed training data and response vector.
+    """
+    # Add "price" to X to make it easier to handle indexes
+    X['price'] = y
+
     X = X.dropna().drop_duplicates()
 
     # Remove rows with negative values for any of the following columns
@@ -52,7 +88,7 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
 
     X = X[(X[non_negative_cols] >= 0).all(axis=1)]
 
-    non_zero_cols = ['date', 'price', 'sqft_lot', 'floors', 'yr_built', 'zipcode', 'sqft_lot15']
+    non_zero_cols = ['price', 'sqft_lot', 'floors', 'yr_built', 'zipcode', 'sqft_lot15']
 
     X = X[(X[non_zero_cols] != 0).all(axis=1)]
 
@@ -68,6 +104,100 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     # Make sure renovation isn't before the house was built
     X = X[(X.yr_built <= X.yr_renovated) | (X.yr_renovated == 0)]
 
+    # Remove outliers in house price
+    extreme_high_price = X['price'].quantile(0.999)
+    extreme_low_price = X['price'].quantile(0.001)
+    X = X[X['price'].between(extreme_low_price, extreme_high_price)]
+
+    # Update preprocess training data for test data before categorical fields, so
+    # all fields exist (yr_built, etc... for the median calculation)
+    train_metadata.col_median_values = X.median(axis=0)
+
+    # Formulate categorical fields (zipcode and years_since_renovation/built)
+    X = _preprocess_categorical_fields(X)
+
+    # Return the prices back to the y column
+    y = X['price'].astype(float)
+    X = X.drop("price", axis=1)
+    return X, y
+
+
+def _preprocess_test_data(X: pd.DataFrame) -> pd.DataFrame:
+    """
+    Preprocess test data (sample matrix). No rows are removed.
+    :param X: DataFrame of shape (n_samples, n_features)
+              Design matrix of regression problem
+    :return: Preprocessed X matrix
+    """
+    # Replace invalid values in rows to ones that make sense.
+    is_zero_or_nan = lambda x: x.isna() | x == 0
+    is_negative_or_nan = lambda x: x.isna() | x.lt(0)
+    is_negative_or_zero_or_nan = lambda x: is_zero_or_nan(x) | is_negative_or_nan(x)
+    is_outside_or_nan = lambda lower, upper: lambda x: (x.isna()) | (x.gt(upper) | x.lt(lower))
+    train_medians = train_metadata.col_median_values
+
+    col_replace_condition = [
+        # Date can be negative if the date is before 1.1.1970
+        ('date', pd.isna, train_medians['date']),
+
+        ('bedrooms', is_negative_or_nan, train_medians['bedrooms']),
+        ('bathrooms', is_negative_or_nan, train_medians['bathrooms']),
+        ('sqft_living', is_negative_or_nan, train_medians['sqft_living']),
+        ('sqft_lot', is_negative_or_nan, train_medians['sqft_lot']),
+        ('floors', is_negative_or_zero_or_nan, train_medians['floors']),
+
+        ('waterfront', is_outside_or_nan(0, 1), 0),
+        ('view', is_outside_or_nan(0, 4), train_medians['view']),
+        ('condition', is_outside_or_nan(1, 5), train_medians['condition']),
+        ('grade', is_outside_or_nan(1, 13), train_medians['grade']),
+
+        ('sqft_above', is_negative_or_nan, train_medians['sqft_above']),
+        ('sqft_basement', is_negative_or_nan, train_medians['sqft_basement']),
+
+        # Assume all houses were built after 1900, and for anything else
+        # give a value of -np.inf, so the year the house was built is set to a long time ago
+        ('yr_built', lambda x: x.isna(), train_medians['yr_built']),
+        ('yr_built', lambda x: x.lt(1900), -np.inf),
+
+        # If the renovation is smaller than 1900, set it to zero
+        ('yr_renovated', lambda x: x.isna() | x.lt(1900), 0),
+
+        # If the zipcode stated in this dataset does not exist in the train dataset (which
+        # deals with illegal zipcodes), it will not appear in the final dataset, so we can ignore it for now.
+
+        ('sqft_living15', is_negative_or_nan, train_medians['sqft_living15']),
+        ('sqft_lot15', is_negative_or_nan, train_medians['sqft_lot15']),
+    ]
+
+    for column, condition, replacement_value in col_replace_condition:
+        condition_mask = condition(X[column])
+        X.loc[condition_mask, column] = replacement_value
+
+    # After fixing all values, make sure that:
+
+    # Sqft sum up correctly, otherwise, change the sqft_living to match the sum of sqft_above + sqft_basement
+
+    invalid_sum_mask = X.sqft_above + X.sqft_basement != X.sqft_living
+    X.loc[invalid_sum_mask, 'sqft_living'] = X.loc[invalid_sum_mask, 'sqft_basement'] + \
+                                             X.loc[invalid_sum_mask, 'sqft_above']
+
+    # If the renovation happened before the house was built, change the renovation year to 0 (not renovated)
+    X.loc[(X.yr_built > X.yr_renovated), 'yr_renovated'] = 0
+
+    # Add the categorical columns and make sure to keep those from the train data/add missing ones
+    X = _preprocess_categorical_fields(X)
+
+    return X
+
+
+def _preprocess_categorical_fields(X: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create one-hot fields for the sample dataframe.
+    Fields added: years_since_renovation, years_since_built, zipcode
+    :param X: DataFrame of shape (n_samples, n_features)
+              Design matrix of regression problem
+    :return: Matrix with the columns.
+    """
     # Fix yr_renovated and yr_built: There is a large difference between "never renovated" (0) and the year renovated
     # (value around 2000), and not so much a difference between renovated in 1950 and 2010.
     # In addition, yr_built and yr_renovated are very closely related to when the actual sale occurred (a house sold
@@ -77,26 +207,25 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     years_since_built = pd.to_datetime(X.date, unit='s').dt.year - X.yr_built
 
     # There can be negative values, for example if a house was sold before it was built.
-    # Using a large range of buckets, to captures all feasible possibilities.
-    bins = np.append(np.linspace(-10, 90, 11), np.inf)
+    # Using only a small amount of features, after experimenting with the best loss.
+    bins = np.append(np.array([-5, 0, 1, 5, 10]), np.inf)
 
-    X["years_since_renovation"] = pd.cut(years_since_renovation, bins=bins, labels=bins[1:], include_lowest=True)
-    X["years_since_built"] = pd.cut(years_since_built, bins=bins, labels=bins[1:], include_lowest=True)
-    X = pd.get_dummies(X, columns=["years_since_renovation"], drop_first=True)
-    X = pd.get_dummies(X, columns=["years_since_built"], drop_first=True)
+    # Note: using .astype(float) to not include all columns if not needed (float to have np.inf).
+    X["years_since_renovation"] = pd.cut(years_since_renovation, bins=bins, labels=bins[1:],
+                                         include_lowest=True).astype(float)
+    X["years_since_built"] = pd.cut(years_since_built, bins=bins, labels=bins[1:],
+                                    include_lowest=True).astype(float)
+
+    # Notice not using the drop_first=True, because we want every zipcode in the train data to have a column,
+    # so missing columns in the test data don't get a column and are ignored.
+    X = pd.get_dummies(X, columns=["years_since_renovation"], drop_first=False, prefix='years_since_renovation_')
+    X = pd.get_dummies(X, columns=["years_since_built"], drop_first=False, prefix='years_since_built_')
     X = X.drop(["yr_renovated", "yr_built"], axis=1)
 
     # Set the categorical variables correctly
-    # No need to fix waterfront/view/grade/condition (because these are correctly sorted)
     X.zipcode = X.zipcode.astype('category')
-    X = pd.get_dummies(X, columns=['zipcode'], drop_first=True)
-
-    # Return the prices back to the y column
-    y = X['price']
-    X = X.drop("price", axis=1)
-
-    # Returning all values as floats
-    return X.astype(float), y.astype(float)
+    X = pd.get_dummies(X, columns=['zipcode'], drop_first=False)
+    return X
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
@@ -136,7 +265,7 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
         if feature.startswith("zipcode"):
             continue
 
-        figure = px.scatter(
+        fig = px.scatter(
             pd.DataFrame({feature: X[feature], y.name: y}),
             x=feature, y=y.name,
             trendline='ols',
@@ -144,7 +273,7 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
             title=f"Correlation between feature: {feature} and {y.name}, "
                   f"<br>Pearson Correlation: {pearson_cor}"
         )
-        figure.write_image(os.path.join(output_path, f"{feature}_to_{y.name}.png"))
+        fig.write_image(os.path.join(output_path, f"{feature}_to_{y.name}.png"))
 
 
 if __name__ == '__main__':
@@ -169,8 +298,11 @@ if __name__ == '__main__':
     #   3) Test fitted model over test set
     #   4) Store average and variance of loss over test set
     # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
+    test_X, _ = preprocess_data(test_X)
 
-    test_X, test_y = preprocess_data(test_X, test_y)
+    # Make sure that test_X has the same columns as the train_X
+    test_X = test_X.reindex(columns=train_X.columns, fill_value=0)
+
     model = LinearRegression()
     plot_df = pd.DataFrame(index=range(10, 101, 1), columns=['loss_mean', 'loss_variance'], dtype=float)
     for p in plot_df.index:
